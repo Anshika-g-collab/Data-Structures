79a65b0d0ff87ef139c2dd447270450bdbea5c15
Anshika-g-collab <anshikapunase@gmail.com>
2025-03-22 13:39:59 +0530

    Check for prime

--- LOOPS-1/Prime.cpp
+++ LOOPS-1/Prime.cpp
@@ -5,17 +5,23 @@ int main(){
     bool f= true;
     cout << "Enter  a number to check: "<<endl;
     cin>>num;
-    for ( int i=2;i<num;i++){
-        if (num%i == 0){
-            f = false;
-            break;
+
+    if (num!=1 and num>1){
+        for ( int i=2;i<num;i++){
+            if (num%i == 0){
+                f = false;
+                break;
+            }
+        }
+        if (f==true) {
+            cout<<"Number is Prime.";
+        }
+        else{
+            cout<<"Number is not Prime.";
         }
     }
-    if (f==true) {
-        cout<<"Number is Prime.";
-    }
     else{
-        cout<<"Number is not Prime.";
+        cout<<" It's neither Prime nor Composite.";
     }
 
 }
