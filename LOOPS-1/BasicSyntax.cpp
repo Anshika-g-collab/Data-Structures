b0b2e9c0c1d89d02bd5e6acdd16c907df6737d5c
Anshika-g-collab <anshikapunase@gmail.com>
2025-03-22 13:09:01 +0530

    Basic Syntax

--- LOOPS-1/BasicSyntax.cpp
+++ LOOPS-1/BasicSyntax.cpp
@@ -1,1 +1,10 @@
-
+# include<iostream>
+using namespace std;
+int main(){
+    // for (int i = 1;i<=5;i++){
+    //     cout<<"Hello Intern!\n";
+    // }
+    for (int i = 1;i<=5;i++){
+        cout<<i<<endl;
+    }
+}
